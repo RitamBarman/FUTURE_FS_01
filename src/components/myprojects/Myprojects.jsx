f613833f1c58259432969820eff9e9a59e2e92a0
Barman <sayantani.barman@dell.com>
2025-07-20 01:09:01 +0530

    Commit 3

--- src/components/myprojects/Myprojects.jsx
+++ src/components/myprojects/Myprojects.jsx
@@ -21,8 +21,8 @@ const Myprojects = () => {
             <h3>My Portfolio(This website)</h3>
             <small className='text-light'>ReactJs</small>
             <div className="portfolio__item-cta">
-              <a href="https://github.com/Thakshila-Bandara/my-portfolio" target="_blank" rel='noreferrer' className='btn'>Github</a>
-              <a href="https://thakshila-bandara.github.io/my-portfolio/" target="_blank" rel='noreferrer' className='btn btn-primary'>Visit Website</a>  
+              <a href="https://github.com/RitamBarman/FUTURE_FS_01" target="_blank" rel='noreferrer' className='btn'>Github</a>
+              <a href="https://ritambarman.github.io/FUTURE_FS_01/" target="_blank" rel='noreferrer' className='btn btn-primary'>Visit Website</a>  
             </div>
             
         </article>
